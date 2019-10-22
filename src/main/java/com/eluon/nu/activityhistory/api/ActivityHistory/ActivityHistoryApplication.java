1a6d4427de3bbe0c2f91b5eb48aefece5645cdd4
ranandadianilham <rananda.d.ilham@gmail.com>
2019-10-22 15:05:08 +0700

    first commit

---
+++ src/main/java/com/eluon/nu/activityhistory/api/ActivityHistory/ActivityHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.eluon.nu.activityhistory.api.ActivityHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eluon/nu/activityhistory/api/ActivityHistory/ActivityHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluon.nu.activityhistory.api.ActivityHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
